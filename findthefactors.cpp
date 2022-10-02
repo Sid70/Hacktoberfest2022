1a0cbd3f20bdc926cbae8a407d0b6ef3741682ed
Siddhanta Choudhury <103874320+Sid70@users.noreply.github.com>
2022-10-02 15:03:14 +0530

    Add files via upload

---
+++ findthefactors.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Factors of " << n << " are \n";
+
+    for ( int i = 1 ; i < n ; i++ )
+    {
+        if ( n % i == 0 )
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
